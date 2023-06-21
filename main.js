c7830957434ba2b2893b07f1f02edfe77e62cfb6
odiljonswe <karimovodilbek008@gmail.com>
2023-06-21 11:07:33 +0500

    vazifa

--- main.js
+++ main.js
@@ -1,110 +1,30 @@
-a = 9;
-if (a==0) {
-    console.log("0 ga teng");
-} else if (a>0) {
-    console.log("true");
-    else{
-        console.log("false")
-    }
-}
-var num = 6;
+console.log(add());
 
-if(num%2!=0){
-    console.log("toq son")
-}else{
-    console.log("juft son")
+function add() {
+  return 3 + 12;
 }
 
-var a=5;
-var a=4;
-if (a>0 && b>0) {
-    console.log("true")
-}else {
-    console.log("false")
+var a = 20;
+
+function checkPositive(a) {
+  if (a > 0) {
+    console.log(true);
+  } else {
+    console.log(false)
+  }
 }
 
-var a=5;
-var b=9;
-var c=12;
+checkPositive(a)
 
-if (a>b && b>c) {
-    console.log("true")
-}else{
-    console.log("false")
-}
-var a=55;
-var b=45;
-var c=33;
+var a = 20;
+var b = 10;
 
-if (a<b && b<c) {
-    console.log("true")
-}else {
-    console.log("false")
+function checkPositive(a) {
+  if (a > 0) {
+    console.log(true);
+  } else {
+    console.log(false)
+  }
 }
 
-var num = 3;
-var num2 = 4;
-
-if(num%2!=0 && num2%2!=0){
-    console.log("true")
-}
-
-var a=5;
-var b=9;
-var c=12;
-var d=15;
-
-if(a>0 && b>0 && c>0 && d>0) {
-    console.log("true")
-}else {
-    console.log ("false")
-}
-
-var a=5;
-var b=9;
-var c=12;
-
-if(a>0 && b>0 || b>0 && c>0 || a>0 && c>0) {
-    console.log("true")
-}else  {
-    console.log("fasle")
-}
-
-var day =;
-
-if(day == 1) {
-    console.log("mon")
-}else if (day == 2) {
-    console.log("tu")
-}
-
-
-
-
-var age = 19;
-if (age >= 18)  {
-    console.log("siz ovoz bera olasiz");
-}else {
-    console.log("siz ovoz bera olmaysiz");
-}
-var a = 2;
-if (a%2==0) {
-    console.log("juft bo'lsin")
-}else {
-    console.log("toq bo'lsin")
-}
-var age = 17;
-var res = age >= 18 ? 'can vote' : 'can not';
-var condition = age > 15;
-var condition2 = age > 18;
-var value = 'can yote';
-var value2 = 'can not';
-var value3 = 'too young';
-var res = condition2 ? value : condition ? value2 : value3;
-console.log(res)
-var age = a>0;
-if (age >=-4) {
-    console.log("musbat")
-}else {
-    console.log("musbat")
-}
+checkPositive(b)
